16fa7aff863e0b9f68ebf2e47a3d7f40d6771f75
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-12-17 15:00:29 -0600

    add tmux

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CONFIG=$HOME/.config
+
 # Copy over all the tmux configurations
-cp -r tmux/ $HOME/.config
+cp -r $DIR/tmux/ $CONFIG
+
+# Setup plugins.
+mkdir -p $CONFIG/tmux/plugins
+
+## Pull tpm.
+git clone https://github.com/tmux-plugins/tpm $CONFIG/tmux/plugins/tpm
