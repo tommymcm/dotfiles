e5f140bf783aae40d4f2ab5813b7bd6f8a1f6c3b
Tommy McMichen <tommy@mcmichen.cc>
2026-01-04 17:01:03 -0500

    fix some tmux stuff

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -11,4 +11,10 @@ cp -r $DIR/tmux/ $CONFIG
 mkdir -p $CONFIG/tmux/plugins
 
 ## Pull tpm.
-git clone https://github.com/tmux-plugins/tpm $CONFIG/tmux/plugins/tpm
+TPM_DIR=$CONFIG/tmux/plugins/tpm
+if [ ! -d "${TPM_DIR}" ] ; then
+    git clone https://github.com/tmux-plugins/tpm ${TPM_DIR}
+fi
+
+# Link the config into home.
+ln -s -f $CONFIG/tmux/tmux.conf $HOME/.tmux.conf
---
+++ tmux/setup-tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv tmux/tmux.conf ~/.config/
